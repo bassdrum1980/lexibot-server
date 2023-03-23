ebc55d4e8eace07874f21225ed0d657df3dd8fb4
Alexander Shevchenko <acidi@icloud.com>
2023-03-22 23:28:26 -0700

    add cors, morgan, bodyParser middlewares

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import * as dotenv from 'dotenv';
 import authRouter from './routes/auth.js';
 
-const app = express();
-const port = process.env.port || 8000;
+dotenv.config();
 
+const app = express();
+const port = process.env.PORT || 8000;
+
+// app middlewares
+app.use(morgan('dev'));
+if ((process.env.NODE_ENV = 'development')) {
+  app.use(cors({ origin: 'http://localhost:3000' }));
+}
+app.use(bodyParser.json());
+
+// route middlewares
 app.use('/api', authRouter);
 
 app.listen(port, () => {
-  console.log(`listening port ${port}...`);
+  console.log(`listening port ${port} - ${process.env.NODE_ENV}`);
 });
