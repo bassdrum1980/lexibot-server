6a5227b9c5be1a49d4839ceee254bbe3069b5043
Alexander Shevchenko <acidi@icloud.com>
2023-05-18 13:11:47 -0700

    updated error message

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -22,7 +22,7 @@ export const protect = (req, res, next) => {
     req.user = user;
     next();
   } catch (error) {
-    console.log('Error: ', error);
+    console.log('ERROR JWT VERIFICATION IN PROTECT', error);
     res.status(401);
     res.json({ error: 'Invalid token' });
     return;
