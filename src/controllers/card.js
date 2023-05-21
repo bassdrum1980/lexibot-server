62f2e79c09ba539ac60e078cf601582de3b9417b
rlyAtas <stas.s.shevchenko@gmail.com>
2023-05-21 17:55:18 +0600

    fix log

--- src/controllers/card.js
+++ src/controllers/card.js
@@ -4,7 +4,7 @@ async function createCard(req, res) {
   let responseStatus;
   let result;
   const { userId, word, attributes } = req.body;
-  console.log(`[contollers/card/postCard] req.body = ${req.body}`);
+  console.log(`[contollers/card/postCard] req.body = ${JSON.stringify(req.body)}`);
 
   const newCard = {
     userId,
@@ -56,7 +56,7 @@ async function updateCard(req, res) {
   const id = Number(req.params.id);
   console.log(`[contollers/card/updateCard] id = ${id}`);
   const { word, attributes, status } = req.body;
-  console.log(`[contollers/card/postCard] req.body = ${req.body}`);
+  console.log(`[contollers/card/postCard] req.body = ${JSON.stringify(req.body)}`);
 
   const where = { id };
   const data = { word, attributes, status };
