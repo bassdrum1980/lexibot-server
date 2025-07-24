0cdebbccf8f3a96f8682dc9329d8f4e5d4217af5
Sasha Shevchenko <acidi@icloud.com>
2025-07-24 16:29:19 -0700

    updates validation (respecting the response data shape)

--- src/validators/article.js
+++ src/validators/article.js
@@ -1,11 +1,11 @@
 import { check } from 'express-validator';
 
 const createArticleValidator = [
-  check('title').not().isEmpty().withMessage('Title is required'),
-  check('content').not().isEmpty().withMessage('Content is required'),
-  check('slug').not().isEmpty().withMessage('Slug is required'),
-  check('tags').isArray().withMessage('Tags must be an array'),
-  check('tags.*').isString().withMessage('Each tag must be a string'),
+  check('article.title').not().isEmpty().withMessage('Title is required'),
+  check('article.content').not().isEmpty().withMessage('Content is required'),
+  check('article.slug').not().isEmpty().withMessage('Slug is required'),
+  check('article.tags').isArray().withMessage('Tags must be an array'),
+  check('article.tags.*').isString().withMessage('Each tag must be a string'),
 ];
 
 export { createArticleValidator };
